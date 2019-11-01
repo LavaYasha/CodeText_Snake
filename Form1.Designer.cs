0457c61293349a995e5783465cce109a1cf1fb23
Yakov Budnikov <dizertir64@gmail.com>
2019-11-01 18:41:12 +0300

    New upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Max
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uLR = new System.Windows.Forms.RadioButton();
+            this.uRL = new System.Windows.Forms.RadioButton();
+            this.bRL = new System.Windows.Forms.RadioButton();
+            this.bLR = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ThisText = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bRU = new System.Windows.Forms.RadioButton();
+            this.bLU = new System.Windows.Forms.RadioButton();
+            this.uRD = new System.Windows.Forms.RadioButton();
+            this.uLD = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uLR
+            // 
+            this.uLR.AutoSize = true;
+            this.uLR.Location = new System.Drawing.Point(16, 15);
+            this.uLR.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.uLR.Name = "uLR";
+            this.uLR.Size = new System.Drawing.Size(179, 21);
+            this.uLR.TabIndex = 0;
+            this.uLR.TabStop = true;
+            this.uLR.Text = "Левый верхний вправо";
+            this.uLR.UseVisualStyleBackColor = true;
+            this.uLR.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // uRL
+            // 
+            this.uRL.AutoSize = true;
+            this.uRL.Location = new System.Drawing.Point(16, 43);
+            this.uRL.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.uRL.Name = "uRL";
+            this.uRL.Size = new System.Drawing.Size(179, 21);
+            this.uRL.TabIndex = 1;
+            this.uRL.TabStop = true;
+            this.uRL.Text = "Правый верхний влево";
+            this.uRL.UseVisualStyleBackColor = true;
+            this.uRL.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // bRL
+            // 
+            this.bRL.AutoSize = true;
+            this.bRL.Location = new System.Drawing.Point(16, 71);
+            this.bRL.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bRL.Name = "bRL";
+            this.bRL.Size = new System.Drawing.Size(175, 21);
+            this.bRL.TabIndex = 3;
+            this.bRL.TabStop = true;
+            this.bRL.Text = "Правый нижний влево";
+            this.bRL.UseVisualStyleBackColor = true;
+            this.bRL.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // bLR
+            // 
+            this.bLR.AutoSize = true;
+            this.bLR.Location = new System.Drawing.Point(16, 100);
+            this.bLR.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bLR.Name = "bLR";
+            this.bLR.Size = new System.Drawing.Size(175, 21);
+            this.bLR.TabIndex = 2;
+            this.bLR.TabStop = true;
+            this.bLR.Text = "Левый нижний вправо";
+            this.bLR.UseVisualStyleBackColor = true;
+            this.bLR.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 214);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Текст";
+            // 
+            // ThisText
+            // 
+            this.ThisText.Location = new System.Drawing.Point(16, 245);
+            this.ThisText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ThisText.Name = "ThisText";
+            this.ThisText.Size = new System.Drawing.Size(565, 344);
+            this.ThisText.TabIndex = 5;
+            this.ThisText.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 209);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Закодировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // bRU
+            // 
+            this.bRU.AutoSize = true;
+            this.bRU.Location = new System.Drawing.Point(213, 71);
+            this.bRU.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bRU.Name = "bRU";
+            this.bRU.Size = new System.Drawing.Size(173, 21);
+            this.bRU.TabIndex = 10;
+            this.bRU.TabStop = true;
+            this.bRU.Text = "Правый нижний вверх";
+            this.bRU.UseVisualStyleBackColor = true;
+            // 
+            // bLU
+            // 
+            this.bLU.AutoSize = true;
+            this.bLU.Location = new System.Drawing.Point(213, 100);
+            this.bLU.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bLU.Name = "bLU";
+            this.bLU.Size = new System.Drawing.Size(165, 21);
+            this.bLU.TabIndex = 9;
+            this.bLU.TabStop = true;
+            this.bLU.Text = "Левый нижний вверх";
+            this.bLU.UseVisualStyleBackColor = true;
+            // 
+            // uRD
+            // 
+            this.uRD.AutoSize = true;
+            this.uRD.Location = new System.Drawing.Point(213, 43);
+            this.uRD.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.uRD.Name = "uRD";
+            this.uRD.Size = new System.Drawing.Size(171, 21);
+            this.uRD.TabIndex = 8;
+            this.uRD.TabStop = true;
+            this.uRD.Text = "Правый верхний вниз";
+            this.uRD.UseVisualStyleBackColor = true;
+            // 
+            // uLD
+            // 
+            this.uLD.AutoSize = true;
+            this.uLD.Location = new System.Drawing.Point(213, 15);
+            this.uLD.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.uLD.Name = "uLD";
+            this.uLD.Size = new System.Drawing.Size(163, 21);
+            this.uLD.TabIndex = 7;
+            this.uLD.TabStop = true;
+            this.uLD.Text = "Левый верхний вниз";
+            this.uLD.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(152, 209);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 28);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Декодировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 604);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bRU);
+            this.Controls.Add(this.bLU);
+            this.Controls.Add(this.uRD);
+            this.Controls.Add(this.uLD);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ThisText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bRL);
+            this.Controls.Add(this.bLR);
+            this.Controls.Add(this.uRL);
+            this.Controls.Add(this.uLR);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton uLR;
+        private System.Windows.Forms.RadioButton uRL;
+        private System.Windows.Forms.RadioButton bRL;
+        private System.Windows.Forms.RadioButton bLR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox ThisText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton bRU;
+        private System.Windows.Forms.RadioButton bLU;
+        private System.Windows.Forms.RadioButton uRD;
+        private System.Windows.Forms.RadioButton uLD;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,434 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Max
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            List<List<char>> matrix = GetMatrix();
+            List<char> complite_text = new List<char>();
+
+            //if (matrix.Count > 1 && matrix[0].Count > 1) {
+            if (uLR.Checked)
+            {
+                complite_text = LeftUpper(matrix, false);
+            }
+            else if (uLD.Checked)
+            {
+                complite_text = LeftUpper(matrix, true);
+            }
+            else if (uRL.Checked)
+            {
+                complite_text = RightUpper(matrix, true);
+            }
+            else if (uRD.Checked)
+            {
+                complite_text = RightUpper(matrix, false);
+            }
+            else if (bRL.Checked)
+            {
+                complite_text = RightBottom(matrix, false);
+            }
+            else if (bRU.Checked)
+            {
+                complite_text = RightBottom(matrix, true);
+            }
+            else if (bLR.Checked)
+            {
+                complite_text = LeftBottom(matrix, true);
+            }
+            else if (bLU.Checked)
+            {
+                complite_text = LeftBottom(matrix, false);
+            }
+            else
+            {
+                MessageBox.Show($"Нужно выбрать способ обхода!");
+            }
+            ThisText.Text = "";
+            for (int i = 0; i < complite_text.Count; i++)
+            {
+                ThisText.Text += complite_text[i];
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            uLR.Checked = true;
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="f"> true == Left to Down    
+        /// false == Left to right</param>
+        /// <returns></returns>
+        private List<char> LeftUpper(List<List<char>> matrix, bool f)
+        {           
+            int rowMax = matrix.Count;
+            int colMax = matrix[0].Count;
+
+            int max = matrix.Count * matrix[0].Count;
+            List<char> code = new List<char>();
+
+            for (int i = 0; i < max; i++)
+            {
+                code.Add(' ');
+            }
+
+            int p = 0;
+            int _col = -1;
+            int _row = -1;
+
+            int _colEven = -1;
+            int counter = 0;
+
+            for (int rows = 0; rows < rowMax; rows++)
+            {
+                if (rows > colMax -1)
+                {
+                    counter++;
+                }
+                for (int columns = 0; columns < rows + 1; columns++)
+                {
+                    if (rows > colMax - 1)
+                    {
+
+                        _col = f ? rows - columns - counter : columns;
+                        _row = f ? columns + counter : rows - _col;
+
+                        if ((rowMax % 2 == 0 && colMax % 2 != 0) || (rowMax % 2 != 0 && colMax % 2 == 0))
+                        {
+                            _colEven = f ? columns : rows - columns - counter;
+                        }
+                    }
+                    else
+                    {
+                        _col = f ? rows - columns : columns;
+                        _row = rows - _col;
+                        if ((rowMax % 2 == 0 && colMax % 2 != 0) || (rowMax % 2 != 0 && colMax % 2 == 0))
+                        {
+                            _colEven = f ? columns : rows - columns;
+                        }
+                    }
+
+                    if (columns > colMax - 1)
+                    {
+                        break;
+                    }
+
+                    code[p] = matrix[_row][_col];
+
+                    if ((rowMax % 2 == 0 && colMax % 2 != 0 )|| (rowMax % 2 != 0 && colMax % 2 == 0))
+                    {
+                        code[max - p - 1] = matrix[rowMax + _colEven - rows - 1][colMax - _colEven - 1];
+                    }
+                    else
+                    {
+                        code[max - p - 1] = matrix[rowMax + _col - rows - 1][colMax - _col - 1];
+                    }
+                    p++;
+                }
+                f = !f;
+            }
+
+            return code;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="f"> true == Right to Up 
+        /// false == Right to Left</param>
+        /// <returns></returns>
+        private List<char> RightBottom(List<List<char>> matrix, bool f)
+        {
+            List<List<char>> temp = new List<List<char>>();
+            int maxRows = matrix.Count;
+            int maxColumns = matrix[0].Count;
+
+            for (int i = 0; i < maxRows; i++)
+            {
+                temp.Add(new List<char>());
+                for (int j = 0; j < maxColumns; j++)
+                {
+                    temp[i].Add(matrix[maxRows - i - 1][maxColumns - j - 1]);
+                }
+            }
+            return LeftUpper(temp, f);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="f"> true == Left to Right    
+        /// false == Left to Up</param>
+        /// <returns></returns>
+        private List<char> LeftBottom(List<List<char>> matrix, bool f)
+        {
+            List<List<char>> temp = new List<List<char>>();
+            int maxRows = matrix.Count;
+            int maxColumns = matrix[0].Count;
+
+            for (int j = 0; j < maxColumns; j++)
+            {
+                temp.Add(new List<char>());
+                for (int i = maxRows - 1; i >= 0; i--)
+                {
+                    temp[j].Add(matrix[i][j]);
+                }
+            }
+            return LeftUpper(temp, f);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <param name="f"> true == Right to Left    
+        /// false == Right to Down</param>
+        /// <returns></returns>
+        private List<char> RightUpper(List<List<char>> matrix, bool f)
+        {
+            List<List<char>> temp = new List<List<char>>();
+            int maxRows = matrix.Count;
+            int maxColumns = matrix[0].Count;
+
+            for (int j = maxColumns - 1; j >= 0; j--)
+            {
+                temp.Add(new List<char>());
+                for (int i = 0; i < maxRows; i++)
+                {
+                    temp[maxColumns - j - 1].Add(matrix[i][j]);
+                }
+            }
+            return LeftUpper(temp, f);
+        }
+
+        //Это не работает...
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            List<List<char>> matrix = GetMatrix();
+            List<char> complite_text = new List<char>();
+
+            if (uLR.Checked)
+            {
+                complite_text = LeftUpperDecode(matrix, false);
+            }
+            else if (uLD.Checked)
+            {
+                complite_text = LeftUpperDecode(matrix, true);
+            }
+            else if (uRL.Checked)
+            {
+                complite_text = RightUpperDecode(matrix, true);
+            }
+            else if (uRD.Checked)
+            {
+                complite_text = RightUpperDecode(matrix, false);
+            }
+            else if (bRL.Checked)
+            {
+                complite_text = LeftUpper(matrix, false);
+            }
+            else if (bRU.Checked)
+            {
+                complite_text = LeftUpper(matrix, true);
+            }
+            else if (bLR.Checked)
+            {
+                complite_text = LeftUpper(matrix, true);
+            }
+            else if (bLU.Checked)
+            {
+                complite_text = LeftUpper(matrix, false);
+            }
+            else
+            {
+                MessageBox.Show($"Нужно выбрать способ обхода!");
+            }
+
+            ThisText.Text = "";
+            for (int i = 0; i < complite_text.Count; i++)
+            {
+                ThisText.Text += complite_text[i];
+            }
+        }
+
+        private List<char> LeftUpperDecode(List<List<char>> matrix, bool f)
+        {
+            List<char> code = new List<char>();
+
+            int max = matrix.Count * matrix[0].Count;
+            int rowMax = matrix.Count;
+            int colMax = matrix[0].Count;
+
+
+            for (int i = 0; i <rowMax; i++)
+            {
+                for (int j = 0; j < colMax; j++)
+                {
+                    code.Add(matrix[i][j]);
+                }
+            }
+
+            int p = 0;
+            int _col = -1;
+            int _row = -1;
+
+            int _colEven = -1;
+            int counter = 0;
+
+            for (int rows = 0; rows < rowMax; rows++)
+            {
+                if (rows > colMax - 1)
+                {
+                    counter++;
+                }
+                for (int columns = 0; columns < rows + 1; columns++)
+                {
+                    if (rows > colMax - 1)
+                    {
+
+                        _col = f ? rows - columns - counter : columns;
+                        _row = f ? columns + counter : rows - _col;
+
+                        if ((rowMax % 2 == 0 && colMax % 2 != 0) || (rowMax % 2 != 0 && colMax % 2 == 0))
+                        {
+                            _colEven = f ? columns : rows - columns - counter;
+                        }
+                    }
+                    else
+                    {
+                        _col = f ? rows - columns : columns;
+                        _row = rows - _col;
+                        if ((rowMax % 2 == 0 && colMax % 2 != 0) || (rowMax % 2 != 0 && colMax % 2 == 0))
+                        {
+                            _colEven = f ? columns : rows - columns;
+                        }
+                    }
+
+                    if (columns > colMax - 1)
+                    {
+                        break;
+                    }
+
+                    matrix[_row][_col] = code[p];
+
+                    if ((rowMax % 2 == 0 && colMax % 2 != 0) || (rowMax % 2 != 0 && colMax % 2 == 0))
+                    {
+                         matrix[rowMax + _colEven - rows - 1][colMax - _colEven - 1] = code[max - p - 1] ;
+                    }
+                    else
+                    {
+                        matrix[rowMax + _col - rows - 1][colMax - _col - 1] = code[max - p - 1] ;
+                    }
+                    p++;
+                }
+                f = !f;
+            }
+
+            code.RemoveRange(0, max);
+            for (int i = 0; i < rowMax; i++)
+            {
+                for (int j = 0; j < colMax; j++)
+                {
+                    code.Add(matrix[i][j]);
+                }
+            }
+
+            return code;
+        }
+
+        private List<char> RightUpperDecode(List<List<char>> matrix, bool f)
+        {
+            List<List<char>> temp = new List<List<char>>();
+            int maxRows = matrix.Count;
+            int maxColumns = matrix[0].Count;
+
+            for (int i = 0; i < maxRows; i++)
+            {
+                temp.Add(new List<char>());
+                for (int j = 0; j < maxColumns; j++)
+                {
+                    temp[i].Add(matrix[maxRows - i - 1][maxColumns - j - 1]);
+                }
+            }
+            return LeftUpperDecode(temp, f);
+        }
+        private List<List<char>> GetMatrix()
+        {
+            string text = ThisText.Text;
+
+
+            int temp = text.Length;
+            int a = 10000;
+            int b = 1;
+            int c = 0;
+
+            do
+            {
+                b++;
+                if (temp % b == 0)
+                {
+                    a = temp / b;
+                    c = b;
+                }
+            } while (a > b);
+
+            List<List<char>> matrix = new List<List<char>>();
+
+            
+
+            int k = 0;
+            if (uLR.Checked || uLD.Checked || bRL.Checked || bRU.Checked)
+            {
+                for (int i = 0; i < c; i++)
+                {
+                    matrix.Add(new List<char>());
+                    for (int j = 0; j < a; j++)
+                    {
+                        matrix[i].Add(text[k]);
+                        k++;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    matrix.Add(new List<char>());
+                    for (int j = 0; j < c; j++)
+                    {
+                        matrix[i].Add(text[k]);
+                        k++;
+                    }
+                }
+            }
+            return matrix;
+        }
+    }
+}
